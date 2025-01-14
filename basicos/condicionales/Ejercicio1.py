0f6fa56893536773517b2098f57e8a846b66d5a2
root <root@parrot.hive.palinuro.cloud>
2025-01-14 10:23:00 +0000

    Python

---
+++ basicos/condicionales/Ejercicio1.py
@@ -0,0 +1,6 @@
+numero = int(input("Dame un número: "))
+
+if numero > 10:
+    print(f"El número {numero} es mayor que 10")
+else:
+    print(f"El número {numero} no es mayor que 10")
---
+++ basicos/condicionales/Ejercicio2.py
@@ -0,0 +1,6 @@
+Años = int(input("Cuántos años tienes: "))
+
+if Años > 18:
+    print("Eres mayor de edad")
+elif Años < 18:
+    print("Eres menor de edad")
