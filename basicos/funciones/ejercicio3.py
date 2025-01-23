f165a866c9697d5ad349aea6bf85d9862733cb91
root <root@parrot.hive.palinuro.cloud>
2025-01-23 12:36:57 +0000

    jijija

---
+++ basicos/funciones/ejercicio3.py
@@ -0,0 +1,6 @@
+def multiplicar(num1, num2):
+    total = num1 * num2
+    print(f"El resultado es {total}")
+    return total
+
+multiplicar(2,6)
---
+++ basicos/funciones/ejercicio4.py
@@ -0,0 +1,9 @@
+def es_par(num1):
+    i=0
+    if num1 %2 == 0:
+        print(f"EL numero es par")
+        return True
+    else:
+        print(f"El numero no es par")
+        return False
+es_par(2)
