0867015312c7a168a72e40a8c1c5526d03e0a6f9
root <root@parrot.hive.palinuro.cloud>
2025-02-13 09:00:02 +0000

    Vinicius

---
+++ basicos/condicionales/Ejercicio8.py
@@ -0,0 +1,6 @@
+contraseña = input("Introduce la contraseña: ")
+
+if contraseña == "secreto123":
+    print("Acceso concedido.")
+else:
+    print("Contraseña incorrecta.")
---
+++ basicos/condicionales/Ejercicio9.py
@@ -0,0 +1,9 @@
+Celsius = float(input("Introduce los grados: "))
+
+if Celsius >= 30:
+    print("Hace calor")
+elif 10 <= Celsius <= 29:
+    print("Temperatura templada")
+else:
+    print("Hace frío")
+
